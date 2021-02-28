a3d36dfb436ff0059759660f1c8c67b499c0f028
Andreas Kjellqvist <linco95@gmail.com>
2021-02-28 21:37:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestingMobile
+
+Developed with Unreal Engine 4
---
+++ Source/TestingMobile/TestingMobileGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestingMobileGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATestingMobileGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATestingMobileGameMode();
+};
+
+
+
